96ef82bee2330cc7244f9ded2774dcffc0252c36
Shakir <shakirkhan72727@gmail.com>
2025-09-28 14:02:45 +0500

    initial commit

---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,21 @@
+import { Suspense } from "react";
+import { Heading } from "@/components/heading";
+import { Spinner } from "@/components/spinner";
+import { TicketList } from "@/features/ticket/components/ticket-list";
+
+// export const dynamic = "force-dynamic";
+// export const revalidate = 15;
+
+const TicketsPage = async () => {
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Heading title="Tickets" description="All your tickets at one place" />
+
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
+    </div>
+  );
+};
+
+export default TicketsPage;
---
+++ src/features/ticket/components/ticket-item.tsx
@@ -0,0 +1,66 @@
+import { Ticket } from "@prisma/client";
+import clsx from "clsx";
+import { LucideArrowUpRightFromSquare, LucideTrash } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
+import { TICKET_ICONS } from "../constants";
+import { getTicket } from "../queries/get-ticket";
+
+type TicketItemProps = {
+  ticket: Ticket;
+  isDetail?: boolean;
+};
+
+const TicketItem = async ({ ticket, isDetail }: TicketItemProps) => {
+  const detailButton = (
+    <Button variant="outline" size="icon" asChild>
+      <Link prefetch href={ticketPath(ticket.id)}>
+        <LucideArrowUpRightFromSquare className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
+
+  const deleteButton = (
+    <form action={deleteTicket.bind(null, ticket.id)}>
+      <Button variant="outline" size="icon">
+        <LucideTrash className="h-4 w-4" />
+      </Button>
+    </form>
+  );
+
+  return (
+    <div
+      className={clsx("w-full flex gap-x-1", {
+        "max-w-[580px]": isDetail,
+        "max-w-[420px]": !isDetail,
+      })}
+    >
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span className="truncate">{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <span
+            className={clsx("whitespace-break-spaces", {
+              "line-clamp-3": !isDetail,
+            })}
+          >
+            {ticket.content}
+          </span>
+        </CardContent>
+      </Card>
+
+      <div className="flex flex-col gap-y-1">
+        {isDetail ? deleteButton : detailButton}
+      </div>
+    </div>
+  );
+};
+
+export { TicketItem };
