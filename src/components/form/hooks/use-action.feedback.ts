e1b209bcdf9764bcab8d0c2652937aa41eb23054
Shakir <shakirkhan72727@gmail.com>
2025-09-30 20:27:17 +0500

    bring custom hook + fix a small bug

---
+++ src/components/form/hooks/use-action.feedback.ts
@@ -0,0 +1,37 @@
+"use client";
+
+import { ActionState } from "./../utils/to-action-state";
+import { useEffect, useRef } from "react";
+
+type OnArgs = {
+  actionState: ActionState;
+};
+
+type useActionFeedbackOptions = {
+  onSuccess?: (onArgs: OnArgs) => void;
+  onError?: (onArgs: OnArgs) => void;
+};
+
+export const useActionFeedback = (
+  actionState: ActionState,
+  options: useActionFeedbackOptions
+) => {
+  const prevTimestamps = useRef(actionState.timestamps);
+  const isUpdate = prevTimestamps.current !== actionState.timestamps;
+
+  useEffect(() => {
+    if (!isUpdate) return;
+    const { message, status } = actionState;
+    if (status === "SUCCESS") {
+      options.onSuccess?.({ actionState });
+    }
+
+    if (status === "ERROR") {
+      if (options.onError) {
+        options.onError?.({ actionState });
+      }
+    }
+
+    prevTimestamps.current = actionState.timestamps;
+  }, [actionState, options, isUpdate]);
+};
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -7,9 +7,10 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { upsertTicket } from "../actions/upsert-ticket";
 import { SubmitButton } from "@/components/form/submit-button";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
 import { FieldError } from "@/components/form/field-error";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
+import { useActionFeedback } from "@/components/form/hooks/use-action.feedback";
 
 /* NOTE FOR SHAKIR:
           - If we want, we can also have a hidden id input field and that will pass that id to formData.
@@ -23,6 +24,15 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormTypes) => {
     upsertTicket.bind(null, ticket?.id),
     EMPTY_ACTION_STATE
   );
+
+  useActionFeedback(actionState, {
+    onSuccess: ({ actionState }) => {
+      console.log(`your msg is nulkl ${actionState.message}`);
+    },
+    onError: ({ actionState }) => {
+      console.log(`your msg is nulkl ${actionState.message}`);
+    },
+  });
 
   return (
     <form action={action} className="flex flex-col gap-y-2">
