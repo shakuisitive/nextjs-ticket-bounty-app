55a643ef7f160b82293fa2a15d584f64d1766971
Shakir <shakirkhan72727@gmail.com>
2025-10-07 15:18:35 +0500

    advanced dropdown ui fix

---
+++ src/components/confirm-dialog.tsx
@@ -0,0 +1,55 @@
+import { cloneElement, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+
+type UseConfirmDialogArgs = {
+  title?: string;
+  description?: string;
+  action: (payload: FormData) => void;
+  trigger: React.ReactElement;
+};
+
+const useConfirmDialog = ({
+  title = "Are you absolutely sure?",
+  description = "This action cannot be undone. Make sure you understand the consequences.",
+  action,
+  trigger,
+}: UseConfirmDialogArgs) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const dialogTrigger = cloneElement(trigger, {
+    onClick: () => setIsOpen((state) => !state),
+  });
+
+  const dialog = (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <form action={action}>
+              <Button type="submit">Confirm</Button>
+            </form>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+
+  return [dialogTrigger, dialog] as const;
+};
+
+export { useConfirmDialog };
