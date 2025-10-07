d7268972d1c5f78919d3af97f67b915c1eabe7e3
Shakir <shakirkhan72727@gmail.com>
2025-10-08 03:18:35 +0500

    exclude sensitive information

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -21,7 +21,13 @@ import { TicketMoreMenu } from "./ticket-more-menu";
 
 type TicketItemProps = {
   ticket: Prisma.TicketGetPayload<{
-    include: { user: true };
+    include: {
+      user: {
+        select: {
+          username: true;
+        };
+      };
+    };
   }>;
   isDetail?: boolean;
 };
