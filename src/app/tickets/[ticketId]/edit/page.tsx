9f866c8309474b4112055dcb0665747087acd15e
Shakir <shakirkhan72727@gmail.com>
2025-10-08 20:46:41 +0500

    implement breadcrumbs

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -4,6 +4,9 @@ import { getAuth } from "@/features/auth/actions/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
+import { Breadcrumbs } from "@/components/breadcrumb";
+import { homePath, ticketPath } from "@/paths";
+import { Separator } from "@/components/ui/separator";
 
 type TicketEditPageProps = {
   params: Promise<{
@@ -24,13 +27,31 @@ const TicketEditPage = async ({ params }: TicketEditPageProps) => {
   }
 
   return (
-    <div className="flex-1 flex flex-col justify-center items-center">
-      <CardCompact
-        title="Edit Ticket"
-        description="Edit an existing ticket"
-        className="w-full max-w-[420px] animate-fade-from-top"
-        content={<TicketUpsertForm ticket={ticket} />}
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            title: "Tickets",
+            href: homePath(),
+          },
+          {
+            title: ticket.title,
+            href: ticketPath(ticket.id),
+          },
+          {
+            title: "Edit",
+          },
+        ]}
       />
+      <Separator />
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <CardCompact
+          title="Edit Ticket"
+          description="Edit an existing ticket"
+          className="w-full max-w-[420px] animate-fade-from-top"
+          content={<TicketUpsertForm ticket={ticket} />}
+        />
+      </div>
     </div>
   );
 };
