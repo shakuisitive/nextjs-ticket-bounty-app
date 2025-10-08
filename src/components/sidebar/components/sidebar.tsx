7b1d001e392695593b7d5e99303806367126d16b
Shakir <shakirkhan72727@gmail.com>
2025-10-09 02:30:56 +0500

    get active path using the library fastest-levenshtein

--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -6,9 +6,17 @@ import { navItems } from "../constants";
 import { SidebarItem } from "./sidebar-item";
 import { useAuth } from "@/features/auth/hooks/use-auth";
 import { usePathname } from "next/navigation";
+import { getActivePath } from "@/utils/get-active-path";
+import { signInPath, signUpPath } from "@/paths";
 
 const Sidebar = () => {
   const { user, isFetched } = useAuth();
+  const pathname = usePathname();
+  const { active, activeIndex } = getActivePath(
+    pathname,
+    navItems.map((item) => item.href),
+    [signInPath(), signUpPath()]
+  );
 
   const [isTransition, setTransition] = useState(false);
   const [isOpen, setOpen] = useState(false);
@@ -36,11 +44,12 @@ const Sidebar = () => {
     >
       <div className="px-3 py-2">
         <nav className="space-y-2">
-          {navItems.map((navItem) => (
+          {navItems.map((navItem, index) => (
             <SidebarItem
               key={navItem.title}
               isOpen={isOpen}
               navItem={navItem}
+              isActive={index === activeIndex}
             />
           ))}
         </nav>
